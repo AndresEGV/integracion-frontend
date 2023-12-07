685ff503573bc13d6c8f1289836c7e6e36b31128
Andres Gomez <andresgomez@workmate.com>
2023-12-07 20:13:07 -0300

    fetch al backend en render

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    const getUsers = async () => {
+      const { data } = await axios("https://integracion-backend.onrender.com/");
+
+      setUsers(data);
+    };
+
+    getUsers();
+  }, []);
+
+  const arrayUsers = users.map((user) => (
+    <li key={user.id}>
+      <p>{user.nombre}</p>
+      <p>{user.email}</p>
+    </li>
+  ));
+
+  return <ul>{arrayUsers}</ul>;
 }
 
-export default App
+export default App;
